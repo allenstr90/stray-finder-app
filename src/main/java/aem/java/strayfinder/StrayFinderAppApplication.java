578ccaea195fc1f418659faf6890194f1bb67489
allenstr90 <allenstr90@gmail.com>
2022-09-13 00:41:49 -0400

    init stray locator app

---
+++ src/main/java/aem/java/strayfinder/StrayFinderAppApplication.java
@@ -0,0 +1,13 @@
+package aem.java.strayfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrayFinderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrayFinderAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aem/java/strayfinder/StrayFinderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package aem.java.strayfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrayFinderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
