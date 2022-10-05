de9a39da2edc3fc72f67edaf392d1abe731da895
allenstr90 <allenstr90@gmail.com>
2022-10-05 08:51:30 -0400

    STRAYFIND-3 #resolve #comment add audit entity

---
+++ src/main/java/aem/java/strayfinder/persistence/model/AuditEntity.java
@@ -0,0 +1,41 @@
+package aem.java.strayfinder.persistence.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.Instant;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public abstract class AuditEntity implements Serializable {
+
+    @CreatedBy
+    @Column(name = "created_by", nullable = false, updatable = false, length = 100)
+    private String createdBy = "anonymous";
+
+    @CreatedDate
+    @Column(name = "created_date", nullable = false, updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss Z")
+    private Instant createdDate = Instant.now();
+
+    @LastModifiedBy
+    @Column(name = "last_modified_by", length = 100)
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    @Column(name = "last_modified_date", nullable = false, updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss Z")
+    private Instant lastModifiedDate = Instant.now();
+}
--- src/main/java/aem/java/strayfinder/persistence/model/Stray.java
+++ src/main/java/aem/java/strayfinder/persistence/model/Stray.java
@@ -1,53 +1,58 @@
 package aem.java.strayfinder.persistence.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 
 @Entity
-public class Stray {
+@Getter
+@Setter
+public class Stray extends AuditEntity implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
+    @Size(min = 3, max = 500)
+    @Column(nullable = false, length = 500)
     private String description;
 
     @NotNull
     @Enumerated(EnumType.STRING)
     private StrayType type;
 
-    public Long getId() {
-        return id;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "stray_tag", joinColumns = @JoinColumn(name = "stray_id"), inverseJoinColumns = @JoinColumn(name = "tag_id"))
+    private Set<Tag> tags = new HashSet<>();
+
+    public void addTag(Tag tag) {
+        this.tags.add(tag);
     }
 
-    public Stray setId(Long id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Stray setDescription(String description) {
-        this.description = description;
-        return this;
-    }
-
-    public StrayType getType() {
-        return type;
-    }
-
-    public Stray setType(StrayType type) {
-        this.type = type;
-        return this;
+    public void removeTag(Tag tag) {
+        this.tags.remove(tag);
     }
 
     @Override
